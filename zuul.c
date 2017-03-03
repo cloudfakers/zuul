89f7727bfcf870b6653fa17f18e90101260aa288
Ignasi Barrera <nacx@apache.org>
2017-03-03 11:39:12 +0100

    Send commands to the channel

--- zuul.c
+++ zuul.c
@@ -37,10 +37,9 @@ void on_nick_in_use(ErrorEvent* event) {
 
 /* IRC callback to open the door */
 void open_door(MessageEvent* event) {
-    irc_message(event->to, "Opening door!");
- 
-    /* Tell Fari to stream the webcam */
-    irc_message("elfari", "afuego http://zuul.bcn.abiquo.com:8080/?action=stream");
+    irc_message(event->to, "Vamos esa puertaaaaaa!!!!");
+    irc_message(event->to, "apm guapi");
+    irc_message(event->to, "afuego http://zuul.bcn.abiquo.com:8080/?action=stream");
     
     digitalWrite(GPIO_PIN, GPIO_ON);
     delay(500);
